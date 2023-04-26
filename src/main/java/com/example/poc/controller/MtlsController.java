9039a698f8cb3af995e891f36a462c82c9e92be9
Leo <45577227+leozanchett@users.noreply.github.com>
2023-04-26 17:13:30 -0300

    Refatorado validação de assinatura

--- src/main/java/com/example/poc/controller/MtlsController.java
+++ src/main/java/com/example/poc/controller/MtlsController.java
@@ -1,21 +1,18 @@
 package com.example.poc.controller;
 
 import com.example.poc.entities.ValidarAssinaturaDigital;
-import org.bouncycastle.math.ec.ECCurve;
-import org.bouncycastle.math.ec.ECPoint;
-import org.bouncycastle.util.encoders.Hex;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigInteger;
+import java.io.UnsupportedEncodingException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SignatureException;
 import java.security.spec.InvalidKeySpecException;
-import java.util.Arrays;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,24 +21,15 @@ import java.util.Map;
 public class MtlsController {
 
     @GetMapping
-    public Map<String, String> mTLS(@RequestHeader("access-signature") String accessSignature, @RequestHeader("mensagem") String mensagem) throws InvalidAlgorithmParameterException, NoSuchAlgorithmException, SignatureException, InvalidKeyException, InvalidKeySpecException {
-//        GerarAssinaturaDigital assinaturaDigital = new GerarAssinaturaDigital();
-//        byte[] assinatura = assinaturaDigital.geraAssinatura(mensagem);
-//        byte[] assinatura = Base64.getDecoder().decode(accessSignature);
+    public Map<String, String> mTLS(@RequestHeader("access-signature") String accessSignature, @RequestHeader("mensagem") String mensagem) throws  NoSuchAlgorithmException, SignatureException, InvalidKeyException, InvalidKeySpecException, UnsupportedEncodingException {
+        byte[] assinaturaBase64 = Base64.getDecoder().decode(accessSignature);
+        ValidarAssinaturaDigital validarAssinaturaDigital = new ValidarAssinaturaDigital(mensagem);
 
-        byte[] assinaturaBytes = Hex.decode(accessSignature);
+        boolean valida = validarAssinaturaDigital.verify(assinaturaBase64);
 
-        ValidarAssinaturaDigital validarAssinaturaDigital = new ValidarAssinaturaDigital();
-//        boolean valida = validarAssinaturaDigital.assinar(assinatura , assinaturaDigital.getPubKey().getEncoded());
-        boolean valida = validarAssinaturaDigital.assinar(assinaturaBytes, validarAssinaturaDigital.getPubKey());
         Map<String, String> response = new HashMap<>();
-        response.put("Assinatura valida? ", String.valueOf(valida));
+        response.put("Assinatura valida", String.valueOf(valida));
         return response;
-    }
-
-    public byte[] tratarFormatoAssinatura(String accessSignature) {
-        byte[] assinaturaBytes = Hex.decode(accessSignature);
-        return assinaturaBytes;
     }
 }
 
--- src/main/java/com/example/poc/entities/ValidarAssinaturaDigital.java
+++ src/main/java/com/example/poc/entities/ValidarAssinaturaDigital.java
@@ -1,5 +1,6 @@
 package com.example.poc.entities;
 
+import java.io.UnsupportedEncodingException;
 import java.security.*;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
@@ -7,31 +8,33 @@ import java.util.Base64;
 
 public class ValidarAssinaturaDigital {
 
-        private  boolean validaAssinatura(byte[] assinatura, String mensagem, PublicKey pubKey) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException, InvalidAlgorithmParameterException {
+        private String mensagem;
+
+        public  ValidarAssinaturaDigital(String mensagem) {
+            this.mensagem = mensagem;
+        }
+
+        public boolean verify(byte[] assinatura) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException, InvalidKeySpecException, UnsupportedEncodingException {
+            System.out.println("Mensagem: " + this.mensagem);
             Signature sig = Signature.getInstance("SHA512withECDSA");
 
             //Inicializando Obj Signature com a Chave Pública
-            sig.initVerify(pubKey);
+            sig.initVerify(this.getPubKey());
 
             //Verificando assinatura
-            sig.update(mensagem.getBytes());
-            return sig.verify(assinatura);
-        }
-        public boolean assinar(byte[] assinatura, byte[] pubKeyBytes) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException, InvalidAlgorithmParameterException, InvalidKeySpecException {
-            String mensagem = "Teste:Mensagem";
-            X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(pubKeyBytes);
-            KeyFactory keyFactory = KeyFactory.getInstance("EC");
-            PublicKey pubKey = keyFactory.generatePublic(pubKeySpec);
-
-            boolean valida = validaAssinatura(assinatura, mensagem, pubKey);
+            sig.update(this.mensagem.getBytes());
+            boolean valida = sig.verify(assinatura);
             System.out.println("Assinatura é válida? " + valida);
             return valida;
         }
 
-        public byte[] getPubKey() {
+        private PublicKey getPubKey() throws NoSuchAlgorithmException, InvalidKeySpecException {
             final  String pubKeyBase64 = "MIGbMBAGByqGSM49AgEGBSuBBAAjA4GGAAQBpnyr74amlFTyNVRhCfWK+3BBQB+DrEwxagd91IYCBaneHLuqGEOooCYgUMaM65NcfJf52auSk5kSjRuBjbbR5ZsANQ2Bj7aEqLPSUmzlHvPnrVX+gCtqyazKnEwwGsJpsGYj/+JXvHFhzPNRnEiM0Al4H2aQGz+kPjPfRUwlf8gPwY8=";
             final  byte[] pubKeyBytes = Base64.getDecoder().decode(pubKeyBase64);
-            return pubKeyBytes;
+            X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(pubKeyBytes);
+            KeyFactory keyFactory = KeyFactory.getInstance("EC");
+            PublicKey pubKey = keyFactory.generatePublic(pubKeySpec);
+            return pubKey;
         }
 
 }
